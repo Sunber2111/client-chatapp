ff555038b32782d318b384fcfd423c58c74df8b8
Sunber2111 <namdang3979@gmail.com>
2020-10-29 14:39:19 +0700

    'test

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-
+const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./build"));
-  app.use("*", express.static("./build"));
+  app.use(express.static("build"));
+  app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
 }
 app.listen(PORT);
