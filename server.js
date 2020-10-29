a1b40b802a3fc50b41b588b389caa1f14de9381e
Sunber2111 <namdang3979@gmail.com>
2020-10-29 15:08:54 +0700

    fix pro

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.use("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
+app.use(express.static("build"));
+app.use("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
 app.listen(PORT);
