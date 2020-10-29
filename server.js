2e872c57352398c900426aaeb5415758118882b5
Sunber2111 <namdang3979@gmail.com>
2020-10-29 15:04:22 +0700

    fix pro

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 
-if (!process.env.NODE_ENV === "production") {
-  console.log("production env");
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
   app.use("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
